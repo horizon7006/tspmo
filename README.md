63c74f61d6451bcf388c188300f55bdf209c0e31
Horizon <mgthedasher@gmail.com>
2025-08-12 19:00:04 +0300

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# ts pmo cli
+
+goated project bruh
+
+yeah nothing much if you wanna learn what it is look at cli.py dummy nah jk its just a slang word meaning thing idk
+(and also uhh ignore the if statements. yeah. HORIZON YOU FRICKING IDIOT)
+
+## HOW TO INSTALL????
+
+bruh just clone the repo (basically this repo) and do "pip install ." thats it now you can do tspmo whatever the hell you want
+
+note: this project is satire lol
---
+++ cli.py
@@ -0,0 +1,37 @@
+import sys
+
+# bruh wtf is this
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: tspmo <slang>")
+        return
+    slang = sys.argv[1]
+    if slang == "ts":
+        print("ts means this shart (i had to censor it god darn it)")
+    elif slang == "pmo":
+        print("pmo means piss me off")
+    elif slang == "icl":
+        print("icl means i can't lie")
+    elif slang == "sm":
+        print("sm means so much")
+    elif slang == "sybau":
+        print("sybau means shut your b ass up")
+    elif slang == "job":
+        print("dude dont say cuss words like that")
+    elif slang == "kevin":
+        print("kevin means something bad")
+    elif slang == "owen":
+        print("owen means something good")
+    elif slang == "gurt":
+        print("yogurt")
+    elif slang == "mango":
+        print("BOII DID YOU SAY MANGO TUFF 67????? MANGO SO TUFF")
+    elif slang == "mustard":
+        print("MUSTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARD")
+    elif slang == "67":
+        print("67 TUFF")
+    else:
+        print(f"bruh what does {slang} mean wtf")
+    
+    if __name__ == "__main__":
+        main()
